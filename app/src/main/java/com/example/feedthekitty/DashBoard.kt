50d6e9dd8ebdeb103f9db00f5d9fde82a83a39eb
Jesse Evan Jacobs <jjacobs9@umd.edu>
2020-11-28 15:25:51 -0500

    Added Login in system

---
+++ app/src/main/java/com/example/feedthekitty/DashBoard.kt
@@ -0,0 +1,15 @@
+package com.example.feedthekitty
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DashBoard : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+
+}
---
+++ app/src/main/java/com/example/feedthekitty/Login.kt
@@ -0,0 +1,59 @@
+package com.example.feedthekitty
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class Login : AppCompatActivity() {
+    private var mDatabaseReference: DatabaseReference? = null
+    private var mDatabase: FirebaseDatabase? = null
+    private var loginBtn: Button? = null
+    private var emailView: EditText? = null
+    private var passView: EditText? = null
+
+    private var mAuth: FirebaseAuth? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login)
+        initializeViews()
+
+        mDatabase = FirebaseDatabase.getInstance()
+        mDatabaseReference = mDatabase!!.reference.child("Users")
+        mAuth = FirebaseAuth.getInstance()
+
+        loginBtn!!.setOnClickListener { loginUserAccount() }
+    }
+
+    private fun loginUserAccount() {
+        //modeled after example materials
+        val email = emailView?.text.toString()
+        val password = passView?.text.toString()
+        val intent = Intent(this, DashBoard::class.java)
+
+        mAuth!!.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if(task.isSuccessful){
+                intent.putExtra(USER_ID, mAuth!!.uid)
+                intent.putExtra(USER_EMAIL, emailView.toString())
+                startActivity(intent)
+
+            }
+        }
+    }
+
+
+    private fun initializeViews() {
+        loginBtn = findViewById(R.id.login)
+        emailView = findViewById(R.id.email)
+        passView = findViewById(R.id.password)
+    }
+
+    companion object {
+        const val USER_EMAIL = "useremail"
+        const val USER_ID = "userid"
+    }
+}
